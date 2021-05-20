c43bedc87441f9e6d9a5e66f183179254a65533a
Miguel Angel Galicia <migel.gt@outlook.com>
2021-05-19 21:28:49 -0500

    add new type to implemet bot interface

--- main.go
+++ main.go
@@ -3,29 +3,50 @@ package main
 import "fmt"
 
 type bot interface {
+	// define a set of methods/functions (behavior)
 	getGreeting() string
 }
 
-type englishBot struct{}
+// Generics Code
+// Dependency Injection
+
+type englishBot struct {
+	name string
+}
+
+func (e englishBot) getGreeting() string {
+	//Very custom logic for generating an english greeting
+	return "hi There!" + e.name
+}
+
+type books []int
+
+func (b books) getGreeting() string {
+	return fmt.Sprintf("hello I am a book, and I have %v books", len(b))
+}
+
 type spanishBot struct{}
 
 func main() {
-	eb := englishBot{}
-	sb := spanishBot{}
 
-	printGreeting(eb)
-	printGreeting(sb)
-}
+	eb := englishBot{
+		name: "golang",
+	}
 
-func printGreeting(b bot) {
-	fmt.Println(b.getGreeting())
-}
+	myBooks := books{1}
+	myBook1 := books{1, 2}
+	myBook2 := books{1, 2, 3}
+	myBook4 := books{1, 2, 3, 5}
 
-func (englishBot) getGreeting() string {
-	//Very custom logic for generating an english greeting
-	return "hi There!"
+	printGreeting(myBooks, myBook1, myBook2, myBook4, eb)
 }
 
 func (spanishBot) getGreeting() string {
 	return "Hola!"
+}
+
+func printGreeting(bots ...bot) {
+	for _, myBot := range bots {
+		fmt.Println(myBot.getGreeting())
+	}
 }
